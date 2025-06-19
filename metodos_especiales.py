1bd662cf1ec9fd33d75c9a9fecfce9a78b3c4b59
Franco <francovalentin_19@hotmail.com>
2025-06-19 19:18:56 -0300

    Modificaciones

--- metodos_especiales.py
+++ metodos_especiales.py
@@ -10,10 +10,18 @@ class persona:
     def __repr__(self):
          return f'persona({self.nombre}, {self.edad})'
     
+    def  __add__(self, otro):
+         nuevo_valor = self.edad + otro.edad
+         return persona(self.nombre+otro.nombre, nuevo_valor)
 
 franco = persona('franco', 29)
+julieta = persona('julieta', 25)
+gustavo = persona('gustavo', 35)
 
 
-repre = repr(franco)
-resultado = eval(repre)
-print(resultado.nombre)
+#repre = repr(franco)
+#resultado = eval(repre)
+#print(resultado.nombre)
+
+nueva_persona = franco+julieta+gustavo
+print(nueva_persona.nombre)
